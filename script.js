ad45b65baff8c0b92d18848ed0060b85bd29b11d
Darci_Rodriigues <dro29558@gmail.com>
2021-10-30 08:09:23 -0300

    5 atualizaçao

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-function menu() {
-    let myMenu = document.getElementById("topNav");
-    if (window.screen.availWidth === "600px") {
-        myMenu.style.display = "flex";
-    } else {
-        myMenu.style.display = "none";
-    }
+let menu = document.getElementById("myMenu")
+document.onclick=mymenu
+function mymenu() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+
+
+
+
+
+
+//const btnMobile = document.getElementById('btn-mobile');
+//console.log(btnMobile)
+//function toggleMenu(event) {
+ // if (event.type === 'touchstart') event.preventDefault();
+ // const nav = document.getElementById("#nav");
+  //nav.classList.toggle('active');
+ 
+ // const active = nav.classList.contains('active');
+  //event.currentTarget.setAttribute//('aria-expanded', active);
+ // if (active) {
+ //   event.currentTarget.setAttribute('aria-label', //'Fechar Menu');
+ // } else {
+ //   event.currentTarget.setAttribute('aria-label', //'Abrir Menu');
+//  }
+//}//
+//btnMobile.addEventListener('click', toggleMenu);//
+//btnMobile.addEventListener('touchstart', toggleMenu);
+//
